8dce43aedfaa34b02cb882b4495ff6e6688210fd
AJ-I-I <136203108+AJ-I-I@users.noreply.github.com>
2023-06-16 19:27:56 -0400

    Add files via upload

---
+++ TimerProgram.java
@@ -0,0 +1,37 @@
+import java.io.*;
+import java.util.Scanner;
+import javax.sound.sampled.*;
+
+public class TimerProgram {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter minutes: ");
+        int minutes = scanner.nextInt();
+
+        System.out.print("Enter seconds: ");
+        int seconds = scanner.nextInt();
+
+        int totalSeconds = minutes * 60 + seconds;
+
+        try {
+            Thread.sleep(totalSeconds * 1000);
+
+            File audioFile = new File("C:/Users/Jones/Downloads/SHADOWWIZARDMONEYGANG.wav");
+            AudioInputStream audioStream = AudioSystem.getAudioInputStream(audioFile);
+            Clip clip = AudioSystem.getClip();
+            clip.open(audioStream);
+            clip.start();
+
+            System.out.println("???");
+
+            // Wait for the audio to finish playing
+            Thread.sleep(clip.getMicrosecondLength() / 1000);
+
+            clip.close();
+            audioStream.close();
+        } catch (InterruptedException | UnsupportedAudioFileException | IOException | LineUnavailableException e) {
+            e.printStackTrace();
+        }
+    }
+}
